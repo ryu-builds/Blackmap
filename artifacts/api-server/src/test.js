95ca9bf48ff0691af688bd82715a00c924f82ea8
Yurei <yurei5dev@gmail.com>
2026-07-19 17:33:28 +0530

    Updated code and fixed issues

---
+++ artifacts/api-server/src/test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var pino_http_1 = require("pino-http");
+var x = (0, pino_http_1.default)();
---
+++ artifacts/api-server/src/test.ts
@@ -0,0 +1,5 @@
+import pinoHttp from "pino-http";
+
+const x = pinoHttp();
+
+export {};
